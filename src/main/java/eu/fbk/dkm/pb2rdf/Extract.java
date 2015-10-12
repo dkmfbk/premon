c7a0e880d8c5da18b92d00f62b51b35b8b9c5d35
Alessio Palmero Aprosio <alessio@apnetwork.it>
2015-10-12 11:37:53 +0200

    WordNet and counters

--- src/main/java/eu/fbk/dkm/pb2rdf/Extract.java
+++ src/main/java/eu/fbk/dkm/pb2rdf/Extract.java
@@ -2,35 +2,50 @@ package eu.fbk.dkm.pb2rdf;
 
 import com.google.common.io.Files;
 import eu.fbk.dkm.pb2rdf.frames.*;
+import eu.fbk.rdfpro.AbstractRDFHandler;
+import eu.fbk.rdfpro.RDFSource;
+import eu.fbk.rdfpro.RDFSources;
 import eu.fbk.rdfpro.util.Namespaces;
 import eu.fbk.rdfpro.util.Statements;
-import org.fbk.cit.hlt.thewikimachine.util.FrequencyHashSet;
 import org.openrdf.model.Statement;
 import org.openrdf.model.URI;
 import org.openrdf.model.impl.ValueFactoryImpl;
+import org.openrdf.model.vocabulary.DCTERMS;
 import org.openrdf.model.vocabulary.RDF;
+import org.openrdf.rio.RDFHandlerException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Extract {
 
 	/*todo:
 
-	- Non mettere i contatori quando ce n'è uno (anche ex1)
 	- Mettere CommandLine
 	- Verificare output
 	- Allineare con WordNet, VerbNet (UbyLemon), FrameNet (UbyLemon)
-	- Decidere per l'ontologia
 
+	- Decidere per l'ontologia
 	- NomBank
 
 	*/
 
 	static final String NAMESPACE = "http://pb2rdf.org/";
+	static final String WN_NAMESPACE = "http://wordnet-rdf.princeton.edu/wn31/";
+	static final Pattern ONTONOTES_FILENAME = Pattern.compile("(.*)-([a-z]+)\\.xml");
+
 	static final ValueFactoryImpl factory = ValueFactoryImpl.getInstance();
+	static final boolean ONLY_VERBS_DEFAULT = true;
+	static final boolean IS_ONTONOTES_DEFAULT = false;
+	static final boolean EXTRACT_EXAMPLES_DEFAULT = false;
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(Extract.class);
 
 	// Bugs!
 	private static HashMap<String, String> bugMap = new HashMap<String, String>();
@@ -86,8 +101,17 @@ public class Extract {
 	public static void main(String[] args) {
 
 		try {
-			File folder = new File("/Users/alessio/Desktop/frames/");
+			File folder = new File("/Users/alessio/Documents/scripts/pb2rdf/data/propbank-1.7");
+			File wnRDF = new File("/Users/alessio/Documents/scripts/pb2rdf/data/wn31.nt");
 			String language = "en";
+			boolean onlyVerbs = ONLY_VERBS_DEFAULT;
+			boolean isOntoNotes = IS_ONTONOTES_DEFAULT;
+			boolean extractExamples = EXTRACT_EXAMPLES_DEFAULT;
+			String onlyOne = "gondola";
+
+			folder = new File("/Users/alessio/Documents/scripts/pb2rdf/data/frames");
+			isOntoNotes = true;
+			extractExamples = true;
 
 			JAXBContext jaxbContext = JAXBContext.newInstance(Frameset.class);
 			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
@@ -99,13 +123,26 @@ public class Extract {
 
 			HashSet<String> roleSetsToIgnore = new HashSet<String>();
 
-			// First tour
-			for (File file : Files.fileTreeTraverser().preOrderTraversal(folder)) {
-				if (file.isDirectory()) {
-					continue;
+			final HashSet<URI> wnURIs = new HashSet<URI>();
+			RDFSource source = RDFSources.read(true, true, null, null, wnRDF.getAbsolutePath());
+			source.emit(new AbstractRDFHandler() {
+				@Override
+				public void handleStatement(Statement statement) throws RDFHandlerException {
+					if (statement.getPredicate().equals(RDF.TYPE) && statement.getObject().equals(LEMON.LEXICAL_ENTRY)) {
+						if (statement.getSubject() instanceof URI) {
+							synchronized (wnURIs) {
+								wnURIs.add((URI) statement.getSubject());
+							}
+						}
+					}
 				}
+			}, 1);
 
-				if (!file.getAbsolutePath().endsWith(".xml")) {
+			// First tour
+			LOGGER.info("Getting list of roles");
+			for (File file : Files.fileTreeTraverser().preOrderTraversal(folder)) {
+
+				if (discardFile(file, onlyVerbs, isOntoNotes)) {
 					continue;
 				}
 
@@ -188,26 +225,66 @@ public class Extract {
 
 
 			// Second tour
+			LOGGER.info("Parsing PropBank files");
 			for (File file : Files.fileTreeTraverser().preOrderTraversal(folder)) {
-				if (file.isDirectory()) {
+
+				if (discardFile(file, onlyVerbs, isOntoNotes)) {
 					continue;
 				}
 
-				if (!file.getAbsolutePath().endsWith(".xml")) {
+				String fileName = file.getName();
+				String lemmaType = "v";
+
+				String lemmaFileName = fileName.replaceAll("\\.xml", "");
+
+				if (isOntoNotes) {
+					Matcher matcher = ONTONOTES_FILENAME.matcher(file.getName());
+					if (matcher.matches()) {
+						lemmaType = matcher.group(2);
+						lemmaFileName = matcher.group(1);
+					}
+					else {
+						throw new Exception("File " + file.getName() + " does not appear to be a good OntoNotes frame file");
+					}
+				}
+
+				if (onlyOne != null && !onlyOne.equals(lemmaFileName)) {
 					continue;
 				}
+
+				LOGGER.info("{} ({})", fileName, lemmaType);
 
 				Frameset frameset = (Frameset) jaxbUnmarshaller.unmarshal(file);
 				List<Object> noteOrPredicate = frameset.getNoteOrPredicate();
 
 				for (Object predicate : noteOrPredicate) {
 					if (predicate instanceof Predicate) {
-						String lemma = ((Predicate) predicate).getLemma();
 						Statement statement;
 
-						URI predicateURI = factory.createURI(NAMESPACE, lemma);
-						statement = factory.createStatement(predicateURI, RDF.TYPE, LEMON.LEXICAL_ENTRY);
-						statements.add(statement);
+						String lemma = ((Predicate) predicate).getLemma().replace('_', '+').replace(' ', '+');
+
+						URI wnURI = factory.createURI(WN_NAMESPACE, lemma + "-" + lemmaType);
+						URI predicateURI;
+						if (wnURIs.contains(wnURI)) {
+							predicateURI = wnURI;
+						}
+						else {
+							LOGGER.debug("Missing frame {}", lemma);
+
+							predicateURI = factory.createURI(NAMESPACE, lemma);
+							statement = factory.createStatement(predicateURI, RDF.TYPE, LEMON.LEXICAL_ENTRY);
+							statements.add(statement);
+
+							// Using this paradigm, there is only one form
+							URI formURI = factory.createURI(NAMESPACE, lemma + "_form");
+							statement = factory.createStatement(predicateURI, LEMON.CANONICAL_FORM, formURI);
+							statements.add(statement);
+							statement = factory.createStatement(formURI, RDF.TYPE, LEMON.FORM);
+							statements.add(statement);
+							statement = factory.createStatement(formURI, LEMON.WRITTEN_REP, factory.createLiteral(lemma, language));
+							statements.add(statement);
+						}
+
 
 						List<Object> noteOrRoleset = ((Predicate) predicate).getNoteOrRoleset();
 						for (Object roleset : noteOrRoleset) {
@@ -225,6 +302,8 @@ public class Extract {
 								//todo: add roleset as a property?
 								statement = factory.createStatement(senseURI, RDF.TYPE, LEMON.LEXICAL_SENSE);
 								statements.add(statement);
+								statement = factory.createStatement(senseURI, DCTERMS.SOURCE, factory.createLiteral(fileName));
+								statements.add(statement);
 								statement = factory.createStatement(predicateURI, LEMON.SENSE, senseURI);
 								statements.add(statement);
 
@@ -235,8 +314,7 @@ public class Extract {
 
 								List<Object> rolesOrExample = ((Roleset) roleset).getNoteOrRolesOrExample();
 
-								//todo: shall we start from 0?
-								int example = 1;
+								List<Example> examples = new ArrayList<Example>();
 
 								for (Object rOrE : rolesOrExample) {
 									if (rOrE instanceof Roles) {
@@ -270,75 +348,99 @@ public class Extract {
 										}
 									}
 
-									if (rOrE instanceof Example) {
-										String text = null;
-										Inflection inflection = null;
+									if (extractExamples) {
+										if (rOrE instanceof Example) {
+											examples.add((Example) rOrE);
+										}
+									}
+								}
 
-										String exType = ((Example) rOrE).getType();
-										String exName = ((Example) rOrE).getName();
-										String exSrc = ((Example) rOrE).getSrc();
+								//todo: shall we start from 0?
+								int example = 1;
 
-										List<Rel> myRels = new ArrayList<Rel>();
-										List<Arg> myArgs = new ArrayList<Arg>();
+								for (Example rOrE : examples) {
+									String text = null;
+									Inflection inflection = null;
 
-										List<Object> exThings = ((Example) rOrE).getInflectionOrNoteOrTextOrArgOrRel();
-										for (Object thing : exThings) {
-											if (thing instanceof Text) {
-												text = ((Text) thing).getvalue().replaceAll("\\s+", " ").trim();
-											}
-											if (thing instanceof Inflection) {
-												inflection = (Inflection) thing;
-											}
+									String exType = rOrE.getType();
+									String exName = rOrE.getName();
+									String exSrc = rOrE.getSrc();
 
-											if (thing instanceof Arg) {
-												myArgs.add((Arg) thing);
-											}
+									List<Rel> myRels = new ArrayList<Rel>();
+									List<Arg> myArgs = new ArrayList<Arg>();
 
-											// Should be one, but it's not defined into the DTD
-											if (thing instanceof Rel) {
-												myRels.add((Rel) thing);
-											}
+									List<Object> exThings = rOrE.getInflectionOrNoteOrTextOrArgOrRel();
+									for (Object thing : exThings) {
+										if (thing instanceof Text) {
+											text = ((Text) thing).getvalue().replaceAll("\\s+", " ").trim();
+										}
+										if (thing instanceof Inflection) {
+											inflection = (Inflection) thing;
 										}
 
-										if (text != null && text.length() > 0) {
+										if (thing instanceof Arg) {
+											myArgs.add((Arg) thing);
+										}
 
-											String exampleStr = rolesetID + "_ex" + example++;
-											URI exampleURI = factory.createURI(NAMESPACE, exampleStr);
+										// Should be one, but it's not defined into the DTD
+										if (thing instanceof Rel) {
+											myRels.add((Rel) thing);
+										}
+									}
 
-											statement = factory.createStatement(exampleURI, RDF.TYPE, LEMON.USAGE_EXAMPLE);
-											statements.add(statement);
-											statement = factory.createStatement(senseURI, LEMON.EXAMPLE, exampleURI);
-											statements.add(statement);
+									if (text != null && text.length() > 0) {
 
-											// Properties
-											addProperty(statements, exampleURI, PB2RDF.PB_EX_NAME, exName, language);
-											addProperty(statements, exampleURI, PB2RDF.PB_EX_SRC, exSrc, language);
-											addProperty(statements, exampleURI, PB2RDF.PB_EX_TYPE, exType, language);
-											addProperty(statements, exampleURI, LEMON.VALUE, text, language);
+										String exampleStr = rolesetID + "_ex" + (examples.size() > 1 ? example++ : "");
+										URI exampleURI = factory.createURI(NAMESPACE, exampleStr);
 
-											FrequencyHashSet<String> argFrequencies = new FrequencyHashSet<String>();
-											for (Arg myArg : myArgs) {
+										statement = factory.createStatement(exampleURI, RDF.TYPE, LEMON.USAGE_EXAMPLE);
+										statements.add(statement);
+										statement = factory.createStatement(senseURI, LEMON.EXAMPLE, exampleURI);
+										statements.add(statement);
 
-												NF nf = new NF(myArg.getN(), myArg.getF());
-												String argName = nf.getArgName();
+										// Properties
+										addProperty(statements, exampleURI, PB2RDF.PB_EX_NAME, exName, language);
+										addProperty(statements, exampleURI, PB2RDF.PB_EX_SRC, exSrc, language);
+										addProperty(statements, exampleURI, PB2RDF.PB_EX_TYPE, exType, language);
+										addProperty(statements, exampleURI, LEMON.VALUE, text, language);
+
+										Map<String, List<Arg>> exampleArgs = new HashMap<String, List<Arg>>();
+										for (Arg myArg : myArgs) {
+
+											NF nf = new NF(myArg.getN(), myArg.getF());
+											String argName = nf.getArgName();
+
+											if (argName == null) {
+												//todo: this should not happen, but it happens
+												continue;
+											}
+											// Bugs!
+											if (bugMap.containsKey(argName)) {
+												argName = bugMap.get(argName);
+											}
+
+											if (!exampleArgs.containsKey(argName)) {
+												exampleArgs.put(argName, new ArrayList<Arg>());
+											}
+											exampleArgs.get(argName).add(myArg);
+										}
+
+										for (Map.Entry<String, List<Arg>> entry : exampleArgs.entrySet()) {
+											String argName = entry.getKey();
+											List<Arg> value = entry.getValue();
+											for (int i = 0; i < value.size(); i++) {
+												Arg myArg = value.get(i);
 												String argValue = myArg.getvalue();
-
-												if (argName == null) {
-													//todo: this should not happen, but it happens
-													continue;
-												}
 												if (argValue == null) {
 													throw new Exception("argValue is null");
 												}
 
-												// Bugs!
-												if (bugMap.containsKey(argName)) {
-													argName = bugMap.get(argName);
+												String addendum = "";
+												if (value.size() > 1) {
+													addendum = "_" + (i + 1);
 												}
 
-												argFrequencies.add(argName);
-
-												URI argURI = factory.createURI(NAMESPACE, exampleStr + "_arg-" + argName + "_" + argFrequencies.get(argName));
+												URI argURI = factory.createURI(NAMESPACE, exampleStr + "_arg-" + argName + addendum);
 
 												statement = factory.createStatement(argURI, RDF.TYPE, PB2RDF.EX_ARG);
 												statements.add(statement);
@@ -349,65 +451,66 @@ public class Extract {
 												statement = factory.createStatement(argURI, PB2RDF.PB_THETA_ROLE, roleStatements.get(argName).getSubject());
 												statements.add(statement);
 											}
+										}
 
-											int relCount = 0;
-											for (Rel myRel : myRels) {
+										for (int i = 0; i < myRels.size(); i++) {
+											Rel myRel = myRels.get(i);
 
-												relCount++;
-												String addendum = "";
-												if (myRels.size() > 1) {
-													addendum += "_" + relCount;
-												}
-
-												NF nf = new NF(null, myRel.getF());
-												String relName = nf.getArgName();
-												String relValue = myRel.getvalue();
-
-												if (relValue == null) {
-													throw new Exception("argValue is null");
-												}
-
-												URI relURI = factory.createURI(NAMESPACE, exampleStr + "_rel" + addendum);
-
-												statement = factory.createStatement(relURI, RDF.TYPE, PB2RDF.EX_REL);
-												statements.add(statement);
-												statement = factory.createStatement(exampleURI, PB2RDF.PB_EX_REL, relURI);
-												statements.add(statement);
-												statement = factory.createStatement(relURI, LEMON.VALUE, factory.createLiteral(relValue, language));
-												statements.add(statement);
-												if (relName != null) {
-													statement = factory.createStatement(relURI, PB2RDF.PB_THETA_ROLE, roleStatements.get(relName).getSubject());
-													statements.add(statement);
-												}
+											String addendum = "";
+											if (myRels.size() > 1) {
+												addendum += "_" + (i + 1);
 											}
 
-											if (inflection != null) {
-												URI inflectionURI = factory.createURI(NAMESPACE, exampleStr + "_inflection");
+											NF nf = new NF(null, myRel.getF());
+											String relName = nf.getArgName();
+											String relValue = myRel.getvalue();
 
-												statement = factory.createStatement(inflectionURI, RDF.TYPE, PB2RDF.INFLECTION);
-												statements.add(statement);
-												statement = factory.createStatement(exampleURI, PB2RDF.PB_EX_INFLECTION, inflectionURI);
-												statements.add(statement);
+											if (relValue == null) {
+												throw new Exception("argValue is null");
+											}
 
-												// Properties
-												addProperty(statements, inflectionURI, PB2RDF.PB_INF_ASPECT, inflection.getAspect(), language);
-												addProperty(statements, inflectionURI, PB2RDF.PB_INF_FORM, inflection.getForm(), language);
-												addProperty(statements, inflectionURI, PB2RDF.PB_INF_PERSON, inflection.getPerson(), language);
-												addProperty(statements, inflectionURI, PB2RDF.PB_INF_TENSE, inflection.getTense(), language);
-												addProperty(statements, inflectionURI, PB2RDF.PB_INF_VOICE, inflection.getVoice(), language);
+											URI relURI = factory.createURI(NAMESPACE, exampleStr + "_rel" + addendum);
+
+											statement = factory.createStatement(relURI, RDF.TYPE, PB2RDF.EX_REL);
+											statements.add(statement);
+											statement = factory.createStatement(exampleURI, PB2RDF.PB_EX_REL, relURI);
+											statements.add(statement);
+											statement = factory.createStatement(relURI, LEMON.VALUE, factory.createLiteral(relValue, language));
+											statements.add(statement);
+											if (relName != null) {
+												statement = factory.createStatement(relURI, PB2RDF.PB_THETA_ROLE, roleStatements.get(relName).getSubject());
+												statements.add(statement);
 											}
 										}
+
+										if (inflection != null) {
+											URI inflectionURI = factory.createURI(NAMESPACE, exampleStr + "_inflection");
+
+											statement = factory.createStatement(inflectionURI, RDF.TYPE, PB2RDF.INFLECTION);
+											statements.add(statement);
+											statement = factory.createStatement(exampleURI, PB2RDF.PB_EX_INFLECTION, inflectionURI);
+											statements.add(statement);
+
+											// Properties
+											addProperty(statements, inflectionURI, PB2RDF.PB_INF_ASPECT, inflection.getAspect(), language);
+											addProperty(statements, inflectionURI, PB2RDF.PB_INF_FORM, inflection.getForm(), language);
+											addProperty(statements, inflectionURI, PB2RDF.PB_INF_PERSON, inflection.getPerson(), language);
+											addProperty(statements, inflectionURI, PB2RDF.PB_INF_TENSE, inflection.getTense(), language);
+											addProperty(statements, inflectionURI, PB2RDF.PB_INF_VOICE, inflection.getVoice(), language);
+										}
 									}
+
 								}
+
 							}
 						}
 					}
 				}
+
 				break;
 			}
 
 			for (Statement statement : statements) {
-//				System.out.println(statement);
 				System.out.println(
 						Statements.formatValue(statement.getSubject(), Namespaces.DEFAULT) + " " +
 								Statements.formatValue(statement.getPredicate(), Namespaces.DEFAULT) + " " +
@@ -420,6 +523,27 @@ public class Extract {
 		}
 
 
+	}
+
+	private static boolean discardFile(File file, boolean onlyVerbs, boolean isOntoNotes) {
+		if (file.isDirectory()) {
+			LOGGER.trace("File {} is a directory", file.getName());
+			return true;
+		}
+
+		if (!file.getAbsolutePath().endsWith(".xml")) {
+			LOGGER.trace("File {} is not XML", file.getName());
+			return true;
+		}
+
+		if (onlyVerbs && isOntoNotes) {
+			if (!file.getAbsolutePath().endsWith("-v.xml")) {
+				LOGGER.trace("File {} is not a verb", file.getName());
+				return true;
+			}
+		}
+
+		return false;
 	}
 
 	private static void addProperty(Collection<Statement> statements, URI uri, URI propertyName, String value, String language) {
--- src/main/java/eu/fbk/dkm/pb2rdf/LEMON.java
+++ src/main/java/eu/fbk/dkm/pb2rdf/LEMON.java
@@ -17,10 +17,13 @@ public class LEMON {
 	static final URI ARGUMENT = factory.createURI(NAMESPACE, "Argument");
 	static final URI SENSE_DEFINITION = factory.createURI(NAMESPACE, "SenseDefinition");
 	static final URI USAGE_EXAMPLE = factory.createURI(NAMESPACE, "UsageExample");
+	static final URI FORM = factory.createURI(NAMESPACE, "Form");
 
 	static final URI SENSE = factory.createURI(NAMESPACE, "sense");
 	static final URI VALUE = factory.createURI(NAMESPACE, "value");
 	static final URI DEFINITION = factory.createURI(NAMESPACE, "definition");
 	static final URI EXAMPLE = factory.createURI(NAMESPACE, "example");
+	static final URI CANONICAL_FORM = factory.createURI(NAMESPACE, "canonicalForm");
+	static final URI WRITTEN_REP = factory.createURI(NAMESPACE, "writtenRep");
 
 }
