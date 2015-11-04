e24d74fc6df84c2f0625443caf72d8354400233f
Francesco Corcoglioniti <francesco.corcoglioniti@gmail.com>
2015-11-04 17:10:38 +0100

    Removed/moved old classes

--- src/test/java/eu/fbk/dkm/premon/framebase/FrameBaseTest.java
+++
@@ -1,9 +0,0 @@
-package eu.fbk.dkm.premon.framebase;
-
-public class FrameBaseTest {
-
-    public static void main(final String[] args) throws Throwable {
-        FrameBase.init(args[0]);
-    }
-
-}
