192ce30499248ac72826130e720f26b431d24f4e
Francesco Corcoglioniti <francesco.corcoglioniti@gmail.com>
2015-11-04 08:15:58 +0100

    Fixed broken links to ontolex module and removed 'visualize with LODE' links

--- owl-to-site.sh
+++ owl-to-site.sh
@@ -5,6 +5,8 @@ for f in {core,pb,nb}; do
     rdfpro @read src/main/owl/prefixes.ttl src/site/resources/ontology/${f}.nt @write src/site/resources/ontology/${f}.ttl @write src/site/resources/ontology/${f}.owl
     curl -F "module=owlapi" -F "caller=http://www.essepuntato.it/lode" -F "filename=$f.ttl" -F "file=@src/site/resources/ontology/$f.ttl" http://www.essepuntato.it/store.php -o src/site/resources/ontology/temp.html -v -H "Expect:" -L
     cat src/site/resources/ontology/temp.html \
+        | sed -E 's/href="http:\/\/www.w3.org\/ns\/lemon\/ontolex"/href="http:\/\/www.w3.org\/community\/ontolex\/wiki\/Final_Model_Specification"/g' \
+        | sed -E 's/\(<a href=[^>]+>visualise it with LODE<\/a>\)//g' \
         | sed -E 's/&lt;((http|file)[^&]+)&gt;/<a href="\1">\&lt;\1\&gt;<\/a>/g' \
         | sed -E 's/<p>[ ]*\*([^<]+)<\/p>/<ul><li>\1<\/li><\/ul>/g' \
         | sed -E 's/<\/ul><ul>//g' \
